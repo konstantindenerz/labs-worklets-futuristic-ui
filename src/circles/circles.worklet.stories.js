7d026d5c0610d1cfefd7d80a3bbe02e49c7918d9
Konstantin Denerz <konstantin.denerz@gmail.com>
2021-04-01 21:57:03 +0200

    build(storybook): enable dark theme for docs, document properties

--- src/circles/circles.worklet.stories.js
+++ src/circles/circles.worklet.stories.js
@@ -28,7 +28,13 @@ export const Default = Template.bind({})
 Default.args = {
     value: 42,
     size: 1.5,
+    /**
+     * Contains a list of circles and the line width.
+     */
     widths: '1px 1px 2px 4px',
+    /**
+     * Contains a list of opacities that refers to the circles defined in widths.
+     */
     opacities: '0.1 0.2 0.5 0.6',
     space: 5
 }
